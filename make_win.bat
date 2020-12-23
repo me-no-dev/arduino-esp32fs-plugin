310e9d0594b35f04f8b9d5a0d9b6ee82d9922db0
lorol <lorolouis@gmail.com>
2020-12-23 12:09:57 -0500

    Update make_win.bat

--- make_win.bat
+++ make_win.bat
@@ -1,9 +1,9 @@
 cd %~dp0\src
 set PATH=your\java\bin;%PATH%
 set ALP=your\arduino\lib
-IF EXIST bin GOTO NOWINDIR
+IF EXIST bin GOTO NODIR
 mkdir bin
-:NOWINDIR
+:NODIR
 del bin\*.jar
 rd /S /Q bin\com
 javac.exe -target 1.8 -cp ".;%ALP%\arduino-core.jar;%ALP%\commons-codec-1.7.jar;%ALP%\pde.jar" -d bin ESP32FS.java
